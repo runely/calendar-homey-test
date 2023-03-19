8302db5b2a44641a0e5bcc7024c694febde323c7
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2023-03-19 22:38:51 +0100

    Added script to get both the raw calendar event and the active event

---
+++ raw.js
@@ -0,0 +1,54 @@
+/**
+ * Homey (SDK3) always runs in UTC. So to run this as close to Homey as possible, launch this with "env TZ=UTC node raw.js" or call the raw script "npm run raw"
+ * 
+ * Remember that "Customized Time Zone" is converted to local timezone which for Homey (SDK3) will be UTC and Africa/Abidjan
+ * 
+ * IcalCalendar will apply the timezone where the Homey is located (chosen in Homey app -> More -> Settings -> Position) to get the correct time for the event
+ * 
+ * However, if an event uses Customized Time Zone, node-ical will treat this as UTC, and IcalCalendar will apply the timezone from the Homey. This will probably not be correct
+ * 
+ * Therefore, when IcalCalendar finds Africa/Abidjan as start tz and Etc/UTC as dtstamp tz, the event will be flagged as not to have the Homey timezone applied. Meaning that the raw time shown in the ics file is what will be used
+ * 
+*/
+(async () => {
+  const nodeIcal = require('node-ical')
+  const getActiveEvents = require('./lib/calendarHomey/get-active-events')
+
+  const config = require('./config')
+  const { warn, error, debug } = config
+
+  const calendars = config.calendars()
+  if (!calendars || !Array.isArray(calendars) || calendars.length === 0) {
+    error('getEvents: Add at least one calendar in calendar.json')
+    process.exit(1)
+  }
+  debug(`raw: Printing ${calendars.length} raw calendars\n`)
+
+  // OPTIONAL to fill out. If filled out, this is the only event which will be logged to console. You should also read "node-ical.update.txt" if you want timezone info for this UID
+  const showOnlyTheseUids = []
+
+  for await (const calendar of calendars) {
+    warn('Fetcing data for', calendar.name)
+    const data = calendar.options.isLocalFile ? nodeIcal.parseFile(calendar.uri) : await nodeIcal.fromURL(calendar.uri)
+
+    let rawEvents
+    if (showOnlyTheseUids.length > 0) {
+      rawEvents = Object.values(data).filter(ev => showOnlyTheseUids.includes(ev.uid))
+      warn('Showing only raw events for UIDs', showOnlyTheseUids)
+      console.log(rawEvents)
+    } else {
+      console.log(data)
+    }
+
+    const activeData = getActiveEvents(calendar.tz, data, calendar.eventLimit, calendar.logProperties, showOnlyTheseUids)
+
+    let activeEvents
+    if (showOnlyTheseUids.length > 0) {
+      activeEvents = activeData.filter(ev => showOnlyTheseUids.find(uid => ev.uid.startsWith(uid)))
+      warn('Showing only active events starting with these UIDs', showOnlyTheseUids)
+      console.log(activeEvents)
+    } else {
+      console.log(activeData)
+    }
+  }
+})()
